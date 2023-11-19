8ca88cce497cd3ec62ed43e6ec6f3d9521749e65
Syahnazz <melfirasyahnazz@gmail.com>
2023-11-19 19:58:13 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#805edb"
+      }
+    },
+  },
+  plugins: [],
+}
+
